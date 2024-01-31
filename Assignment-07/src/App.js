4e60a8c981e6b947cbcab1cca84151ee4c13b407
Rutuja Chandgude <rutuchandgude@gmail.com>
2024-01-31 09:38:59 +0530

    Assignment 07

--- Assignment-07/src/App.js
+++ Assignment-07/src/App.js
@@ -4,17 +4,17 @@ import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
 import RestaurantCard from "./components/RestaurantCard";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Error from "./components/Error";
+import { createBrowserRouter, RouterProvider,Outlet } from "react-router-dom";
 
 
 function AppLayout() {
     return (
         <React.Fragment>
             <Header />
-            <Body />
+            <Outlet />
         </React.Fragment>
     );
 }
@@ -23,16 +23,23 @@ const appRouter = createBrowserRouter([
     {
         path:"/",
         element : <AppLayout/>,
-        errorElement : <Error/>,
-    },
-    {
-        path:"/about",
-        element : <About />,
-    },
-    {
-        path:"/contact",
-        element : <Contact />,
-    }
+        children : [
+        {
+            path:"/",
+            element : <Body/>,
+        },
+        {
+            path:"/about",
+            element : <About />,
+        },
+        {
+            path:"/contact",
+            element : <Contact />,
+        }
+    ],
+    errorElement : <Error/>,
+},
+  
 ]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
