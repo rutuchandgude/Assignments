7de99f00ffdda8c587c1f565c339418c897f5161
Rutuja Chandgude <rutuchandgude@gmail.com>
2024-02-04 22:38:47 +0530

    adding new API for RestaurantMenu

--- Assignment-07/src/components/RestaurantMenu.js
+++ Assignment-07/src/components/RestaurantMenu.js
@@ -1,7 +1,8 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import Shimmer from "./Shimmer";
 
 const RestaurantMenu = () =>{
-
+const [resInfo,setResInfo] = useState(null);
     useEffect(()=>{
      fetchMenu();
     },[]);
@@ -9,15 +10,22 @@ const RestaurantMenu = () =>{
     const fetchMenu = async () =>{
         const data = await fetch(
       
-  'https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9715987&lng=77.5945627&restaurantId='
+  'https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9351929&lng=77.62448069999999&restaurantId=229&catalog_qa=undefined&submitAction=ENTER'
         );
         const json = await data.json();
         console.log(json);
+        setResInfo(json.data);
     };
 
-    return(
+//    const {name} = resInfo?.cards[0]?.card?.card?.info;
+
+    return resInfo === null ? (
+    <Shimmer/>
+    ):(
  <div className="menu">
- <h1>Name of the Restaurant</h1>
+ <h1>Name of the restaurant</h1>
+ {/* <h3>{cuisines.join(",")}</h3>
+ <h3>{costForTwoMessage}</h3> */}
  <h2>Menu</h2>
  <ul>
     <li>Biryani</li>
