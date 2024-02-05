d048f90f4803cf9fe3ac3911b0e67f4694d6e35e
Rutuja Chandgude <rutuchandgude@gmail.com>
2024-02-05 22:50:42 +0530

    Assignment-07 completed

--- Assignment-07/src/components/Body.js
+++ Assignment-07/src/components/Body.js
@@ -2,6 +2,7 @@ import RestaurantCard from "./RestaurantCard";
 import { useEffect, useState } from "react";
 import swiggy_API_URL from "../utils/constants";
 import Shimmer from "./Shimmer";
+import { Link } from "react-router-dom";
 
 const Body = () => {
 
@@ -65,7 +66,9 @@ return listOfRestaurants.length === 0? (
               </div>
             <div className="res-container">
               {filteredRestaurant.map((restaurant)=>(
-                 <RestaurantCard key={restaurant.info.id} resData={restaurant} />
+                <Link
+                key={restaurant.info.id}
+                 to={"/restaurants/" + restaurant.info.id}><RestaurantCard  resData={restaurant} /></Link> 
               ))}
             </div>
         </div>
--- Assignment-07/src/components/RestaurantMenu.js
+++ Assignment-07/src/components/RestaurantMenu.js
@@ -1,38 +1,52 @@
 import { useEffect, useState } from "react";
 import Shimmer from "./Shimmer";
+import { useParams } from "react-router-dom";
+import { Menu_API } from "../utils/constants";
 
-const RestaurantMenu = () =>{
-const [resInfo,setResInfo] = useState(null);
-    useEffect(()=>{
-     fetchMenu();
-    },[]);
+const RestaurantMenu = () => {
+  const [resInfo, setResInfo] = useState(null);
 
-    const fetchMenu = async () =>{
-        const data = await fetch(
-      
-  'https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9351929&lng=77.62448069999999&restaurantId=229&catalog_qa=undefined&submitAction=ENTER'
-        );
-        const json = await data.json();
-        console.log(json);
-        setResInfo(json.data);
-    };
+  const {resId } = useParams();
+//   console.log(params);
 
-//    const {name} = resInfo?.cards[0]?.card?.card?.info;
+  useEffect(() => {
+    fetchMenu();
+  }, []);
 
-    return resInfo === null ? (
-    <Shimmer/>
-    ):(
- <div className="menu">
- <h1>Name of the restaurant</h1>
- {/* <h3>{cuisines.join(",")}</h3>
- <h3>{costForTwoMessage}</h3> */}
- <h2>Menu</h2>
- <ul>
-    <li>Biryani</li>
-    <li>Burgers</li>
-    <li>Diet Cock</li>
- </ul>
- </div>
-    );
+  const fetchMenu = async () => {
+    const data = await fetch(
+      Menu_API + resId );
+    const json = await data.json();
+    console.log(json);
+    setResInfo(json.data);
+  };
+
+  if (resInfo === null) return <Shimmer />;
+
+  const { name, cuisines, costForTwoMessage } =
+    resInfo?.cards[0]?.card?.card?.info;
+
+  const { itemCards } =
+    resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card;
+
+  console.log(itemCards);
+
+  return (
+    <div className="menu">
+      <h1>{name}</h1>
+      <p>
+        {cuisines.join(", ")} - {costForTwoMessage}
+      </p>
+      <h2>Menu</h2>
+      <ul>
+        {itemCards.map((item) => (
+          <li key={item.card.info.id}>
+            {item.card.info.name} - {"Rs."}
+            {item.card.info.price / 100 || item.card.info.defaultprice / 100}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 export default RestaurantMenu;
