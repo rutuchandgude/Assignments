9861b78b58148c490dc2b48373ccc4b951319ccf
Rutuja Chandgude <rutuchandgude@gmail.com>
2024-02-06 22:27:01 +0530

    Assignment-08 started

---
+++ Assignment-08/src/components/About.js
@@ -0,0 +1,14 @@
+import User from "./User";
+import UserClass from "./UserClass";
+
+const About = () =>{
+    return (
+        <div>
+           <h1>About page</h1>
+           <h2>This is About US page</h2>
+           <User name={"Rutuja Chandgude(Function)"}/>
+           <UserClass name={"Rutuja Chandgude(Class)"} location={"Pune Class"}/>
+       </div>
+    )
+}
+export default About;
---
+++ Assignment-08/src/components/User.js
@@ -0,0 +1,10 @@
+const User = ({name}) =>{
+    return (
+     <div className="user-card">
+     <h2>Name:{name}</h2>
+     <h3>Location:pune</h3>
+     <h4>Contact:@rutuchandgude</h4>
+     </div>
+    );
+};
+export default User ;
