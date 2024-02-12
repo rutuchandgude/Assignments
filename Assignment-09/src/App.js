c0705a712c3129baa760f319f51037eed65e5bc4
Rutuja Chandgude <rutuchandgude@gmail.com>
2024-02-12 23:52:47 +0530

    Episode-09 Completed

--- Assignment-09/src/App.js
+++ Assignment-09/src/App.js
@@ -1,5 +1,5 @@
 
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
@@ -9,7 +9,16 @@ import Contact from "./components/Contact";
 import Error from "./components/Error";
 import { createBrowserRouter, RouterProvider,Outlet } from "react-router-dom";
 import RestaurantMenu from "./components/RestaurantMenu";
+// import Grocery from "./components/Grocery";
 
+//Chunking
+//Code Splitting
+//Dynamic Bunding
+//lazy loading
+//on demand loading
+//dynamic import
+
+const Grocery = lazy(()=> import("./components/Grocery"));
 
 function AppLayout() {
     return (
@@ -22,9 +31,9 @@ function AppLayout() {
 
 const appRouter = createBrowserRouter([
     {
-        path:"/",
-        element : <AppLayout/>,
-        children : [
+         path:"/",
+         element : <AppLayout/>,
+         children : [
         {
             path:"/",
             element : <Body/>,
@@ -36,6 +45,14 @@ const appRouter = createBrowserRouter([
         {
             path:"/contact",
             element : <Contact />,
+        },
+        {
+            path:"/grocery",
+            element :(
+            <Suspense fallback={<h1>Loading...</h1>}>
+                 <Grocery /> 
+            </Suspense> 
+            ),
         },
         {
             path:"/restaurants/:resId",
