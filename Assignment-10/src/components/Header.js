eb57e9a45951a88a3190d1eb733293a00e4daae1
Rutuja Chandgude <rutuchandgude@gmail.com>
2024-02-14 23:51:27 +0530

    Episode-10

---
+++ Assignment-10/src/components/Header.js
@@ -0,0 +1,53 @@
+import { LOGO_URL } from "../utils/constants";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import useOnlineStatus from "../utils/useOnlineStatus";
+
+const Header = () => {
+  const [btnNameReact, setBtnNameReact] = useState("Login");
+  const onlineStatus = useOnlineStatus();
+  //If no dependency array => useEffect is called on every render
+  //If dependency array is empty = [] => useEffect is called on initial render(just once)
+  //If dependency array is [btnNameReact] => called everytime btnNameReact is updated.
+  // useEffect(()=>{
+  //     console.log("useEffect called");
+  // },btnNameReact);
+
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img className="logo" src={LOGO_URL} />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Online Status : {onlineStatus ? "✅" : "🔴"}</li>
+          <li to="/">
+            <Link>Home</Link>
+          </li>
+          <li>
+            <Link to="/About">About Us</Link>
+          </li>
+          <li>
+            <Link to="/Contact">Contact Us</Link>
+          </li>
+          <li>
+            <Link to="/grocery">Grocery</Link>
+          </li>
+          <li>Cart</li>
+          <button
+            className="login"
+            onClick={() => {
+              btnNameReact === "Login"
+                ? setBtnNameReact("Logout")
+                : setBtnNameReact("Login");
+            }}
+          >
+            {btnNameReact}
+          </button>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
