9d3a52ed97c6a01629bbcf730c239b9b088d3757
Rutuja Chandgude <rutuchandgude@gmail.com>
2024-02-21 23:28:55 +0530

    Episode-11

---
+++ Assignment-11/src/components/RestaurantCard.js
@@ -0,0 +1,40 @@
+import { CDN_URL } from "../utils/constants";
+
+const RestaurantCard = (props) => {
+  const { resData } = props;
+
+  const { cloudinaryImageId, name, cuisines, avgRating, costForTwo, sla } =
+    resData?.info;
+  return (
+    <div className="m-4 p-4 w-[240px] rounded-lg bg-gray-100 hover:bg-gray-200">
+      <img
+        className="rounded-lg"
+        alt="res-logo"
+        src={CDN_URL + cloudinaryImageId}
+      />
+      <h3 className="font-bold py-4 text-lg">{name}</h3>
+      <h4>{cuisines.join(", ")}</h4>
+      <h4>{avgRating}</h4>
+      <h4>{costForTwo}</h4>
+      <span>
+        <h4>{sla?.slaString}</h4>
+      </span>
+    </div>
+  );
+};
+
+//Higher Order Component
+//input - RestaurantCard => RestaurantCardPromoted
+
+export const withPromtedLabel = (RestaurantCard) => {
+  return (props) => {
+    return (
+      <div>
+        <label className="absolute bg-black text-white m-2 p-2 rounded-lg">Promoted</label>
+        <RestaurantCard  {...props}/>
+      </div>
+    );
+  };
+};
+
+export default RestaurantCard;
